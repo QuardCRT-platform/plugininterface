6bdc98e08111dfc2468b2e4907467e2367242a1d
xiaoming <2014500726@smail.xtu.edu.cn>
2023-12-14 11:07:02 +0800

    init project
    Signed-off-by: xiaoming <2014500726@smail.xtu.edu.cn>

---
+++ plugininterface.h
@@ -0,0 +1,39 @@
+#ifndef PLUGININTERFACE_H
+#define PLUGININTERFACE_H
+
+#include <QtPlugin>
+#include <QObject>
+#include <QMap>
+#include <QMenu>
+#include <QAction>
+#include <QVariant>
+#include <QLocale>
+#include <QList>
+
+class PluginInterface : public QObject
+{
+    Q_OBJECT
+public:
+    virtual ~PluginInterface() {}
+    virtual int init(QMap<QString, QString> params, QWidget *parent) = 0;
+    virtual QString name() = 0;
+    virtual QString version() = 0;
+    virtual QMenu *mainMenu() = 0;
+    virtual QAction *mainAction() = 0;
+    virtual QMenu *terminalContextMenu(QString selectedText, QString workingDirectory, QMenu *parentMenu) = 0;
+    virtual QList<QAction *> terminalContextAction(QString selectedText, QString workingDirectory, QMenu *parentMenu) = 0;
+    virtual void setLanguage(const QLocale &language,QApplication *app) = 0;
+    virtual void retranslateUi() = 0;
+
+signals:
+    void requestSSH2Connect(QString host, QString user, QString password, int port);
+    void sendCommand(QString cmd);
+    void writeSettings(QString path, QString key, QVariant value);
+    void readSettings(QString path, QString key, QVariant &value);
+};
+
+#define PluginInterface_iid "org.quardCRT.PluginInterface"
+
+Q_DECLARE_INTERFACE(PluginInterface, PluginInterface_iid)
+
+#endif // PLUGININTERFACE_H
